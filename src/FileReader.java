96df5bbfe7416688b96d4b162d09a38af5b0ee79
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2018-01-27 10:54:36 -0600

    FIrst Commit

---
+++ src/FileReader.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class FileReader {
+
+    public static void main(String[] args){
+
+        java.io.File file = new java.io.File("ThisFile.txt");
+        try {
+            Scanner input = new Scanner(file);
+            while(input.hasNext()){
+                String num = input.nextLine();
+                System.out.println(num);
+            }
+        }
+
+    }
+}
